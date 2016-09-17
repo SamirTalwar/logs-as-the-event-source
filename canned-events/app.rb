26eba2d9b308c461808abf2e9c8ce6d5eea81681
Samir Talwar <samir@noodlesandwich.com>
2016-09-17 16:57:30 +0100

    Never stop.

--- canned-events/app.rb
+++ canned-events/app.rb
@@ -6,9 +6,11 @@ require 'yaml'
 events = YAML.load_file(ARGV[0])['events']
 time = 0
 events.each do |event|
-  sleep (event['timestamp'] - time)
+  sleep(event['timestamp'] - time)
   time = event['timestamp']
   event.delete('timestamp')
   puts event.to_json
   $stdout.flush
 end
+
+sleep(60 * 60 * 24)
