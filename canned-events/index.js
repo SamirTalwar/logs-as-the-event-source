eddb53a292fd445536b6d93204da490f8c18646a
Samir Talwar <samir@noodlesandwich.com>
2016-09-20 22:56:31 +0100

    Rewrite the canned events server in node.

---
+++ canned-events/index.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+
+const fs = require('fs')
+const yaml = require('js-yaml')
+
+main = () => {
+  fs.readFile(process.argv[2], (error, contents) => {
+    if (error) {
+      console.error(error)
+      process.exitCode = 1
+      return
+    }
+
+    const events = yaml.safeLoad(contents).events
+    emit(events, 0)
+  })
+}
+
+const emit = (events, time) => {
+  const event = events[0]
+  if (!event) {
+    return
+  }
+
+  setTimeout(() => {
+    let currentTime = event.timestamp
+    delete event.timestamp
+    console.log(JSON.stringify(event))
+
+    const rest = events.slice(1)
+    emit(rest, currentTime)
+  }, (event.timestamp - time) * 1000)
+}
+
+main()
+
+setTimeout(() => {}, 1000 * 60 * 60 * 24)
